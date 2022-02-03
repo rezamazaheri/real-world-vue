df293c8a7ccd8d307c91706419b6782c77956266
Reza Mazaheri <rezamazaheri.email@gmail.com>
2022-02-03 11:34:27 -0800

    Using Axios To Making API Calls

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -6,9 +6,21 @@
 
 <script setup lang="ts">
 import EventCard from '../components/EventCard.vue';
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+const BASE_URL = 'https://my-json-server.typicode.com/rezamazaheri/real-world-vue/'
 
-const events = ref(null)
+const events = ref([])
+
+onMounted( async() => {
+    try {
+        const { data } = await axios.get(BASE_URL + 'events')
+        events.value = data
+    }
+    catch(error) {
+        throw error
+    }
+})
 </script>
 
 <style lang="less">
