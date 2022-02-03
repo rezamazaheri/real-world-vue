5c4a79163199b53cf8efb8a59a196a5d05bfb2ea
Reza Mazaheri <rezamazaheri.email@gmail.com>
2022-02-02 17:52:35 -0800

    Poplate Main Vue File to Show Events and Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,45 @@
-
 <!--
 ===================================
-Main Vue File
+Main Vue File(Root Component)
 ===================================
 -->
 
 <template>
+<div id="app">
+	<div id="nav">
+		<router-link :to="{ name: 'Home' }">Home</router-link> |
+		<router-link :to="{ name: 'About' }">About</router-link>
+	</div>
+	<h1>Events For Good</h1>
+	<router-view></router-view>
+</div>
 </template>
 
 <script setup lang="ts">
 </script>
 
-<style>
+<style lang="less">
+@main-color: #2c3e50;
+@link-color: #2c3e50;
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: @main-color;
+    margin-top: 4rem;
+}
+
+#nav {
+    padding: 2rem;
+    a {
+        font-weight: bold;
+        color: @link-color;
+    } 
+
+}
+
+h4 {
+    font-size: 1.5rem;
 }
 </style>
