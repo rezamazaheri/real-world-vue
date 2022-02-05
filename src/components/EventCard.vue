ce15152bf416e5d811c870a1aa8a22ff7a3451b3
Reza Mazaheri <rezamazaheri.email@gmail.com>
2022-02-04 21:34:56 -0800

    Add router-link For Navigation

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -5,27 +5,27 @@ Building Block of the App
 -->
 
 <template>
-    <div class="event-card">
-        <span>@ {{ event.time }} on {{ event.date }}</span>
-        <h4>{{ event.title }}</h4>
-    </div>
+    <router-link class="event-link" :to="{ name: 'EventDetails', params: { id: event?.id } }">
+        <div class="event-card" v-if="event">
+            <span>@ {{ event.time }} on {{ event.date }}</span>
+            <h4>{{ event.title }}</h4>
+        </div>
+    </router-link>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue'
 import { Event } from './model/Event'
 
 const props = defineProps({
     event: Object as () => Event
 })
-
 </script>
 
 <style lang="less">
 @card-color: #39495c;
+@link-color: #2c3e50;
 .event-card {
     padding: 1rem;
-    width: 30%;
     cursor: pointer;
     border: 1px solid @card-color;
     margin-bottom: 0.5rem;
@@ -33,5 +33,11 @@ const props = defineProps({
         transform: scale(1.01);
         box-shadow: 0 3px 12px rgba(0, 0, 0, 0.2);
     }
+}
+
+.event-link {
+    color: @link-color;
+    width: 50%;
+    text-decoration: none;
 }
 </style>
