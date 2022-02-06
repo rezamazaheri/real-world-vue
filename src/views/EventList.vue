729d5280bf31d27b94011a31733e4def05d82f86
Reza Mazaheri <rezamazaheri.email@gmail.com>
2022-02-05 17:40:18 -0800

    Pass On page Number To API, Add Pagination

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,15 +1,24 @@
 <template>
     <div class="home" v-if="events">
         <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
+        <router-link :to="{ name: 'EventList', query: { page: page - 1} }"
+            rel="prev"
+            v-if="page != 1">
+            Pre Page</router-link>
+
+        <router-link :to="{ name: 'EventList', query: { page: page + 1} }"
+            rel="next"
+            v-if="hasNextPage">
+            Next Page</router-link>    
     </div>
 </template>
 
 <script setup lang="ts">
 import EventCard from '../components/EventCard.vue'
-import { ref, onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import EventService from '../services/EventService'
 
-const events = ref([{
+let events = ref([{
     "id": 1001,
     "category": "",
     "title": "",
@@ -20,13 +29,30 @@ const events = ref([{
     "organizer": ""
 }])
 
+const props = defineProps({
+    page :{
+        type: Number,
+        required: true
+    }
+})
+
+const PER_PAGE = 2
+
 onMounted(async() => {
-    try {
-        const { data } = await EventService.getEvents()
-        events.value = data
+    try {  
+        const response = await EventService.getEvents(PER_PAGE, props.page)
+        events.value = response.data
+        totalEvents.value = Number(response.headers['x-total-count'])
     } catch(error) {
         throw error
     }
+})
+
+let totalEvents = ref(0)
+
+const hasNextPage = computed(() => {
+    let totalPages = Math.ceil(totalEvents.value / PER_PAGE)
+    return props.page < totalPages
 })
 </script>
 
